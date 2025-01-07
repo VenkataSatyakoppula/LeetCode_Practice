422fa4195347252ab362584abe0e93ff0125382d
VenkataSatyakoppula <satya5202@gmail.com>
2025-01-06 19:04:30 -0500

    01-06-2025

---
+++ arrays_and_hashing/easy_concatination_of_array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        arrLen = len(nums)
+        for i in range(arrLen):
+            nums.append(nums[i])
+        return nums
